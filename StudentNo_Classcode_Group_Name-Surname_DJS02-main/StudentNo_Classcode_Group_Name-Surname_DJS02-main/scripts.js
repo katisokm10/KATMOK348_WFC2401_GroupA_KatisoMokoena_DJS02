9563b420543a1f5dec44fec30f47ee5c7495562d
katisokm10 <130474037+katisokm10@users.noreply.github.com>
2024-05-10 13:27:48 +0200

    debugged script.js

--- StudentNo_Classcode_Group_Name-Surname_DJS02-main/StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -5,5 +5,25 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  if(isNaN(dividend) || isNaN(divider)){
+    document.body.innerHTML = `<h1>Something went critically wrong. Please reload the page.</h1>`
+    console.error('Error: Invaild number');
+
+  }
+  if(dividend === ""|| divider === ""){
+    result.innerText ="Division not performed. Both values are required in inputs. Try again"
+    
+  }else{
+    if(divider == 0){
+      console.log(divider)
+      result.innerText = 'Division not performed. Invalid number provided. Try again'
+      console.error('Error: Divider cannot be zero');
+    }else{
+      if(!isNaN(dividend) || !isNaN(divider))
+      result.innerText = Math.floor(dividend / divider);
+    }
+    
+    
+  }
 });
